9fe5e97bed26ebba0d55731254e980db45d72697
P1n3appl3 <P1n3appl3@users.noreply.github.com>
2021-11-13 10:06:53 +0000

    deploy: 16aa0421bcbf95f5b764387938ef6d707724fb84

---
+++ implementors/core/marker/trait.Copy.js
@@ -0,0 +1,4 @@
+(function() {var implementors = {};
+implementors["bytemuck"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.56.1/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"bytemuck/enum.PodCastError.html\" title=\"enum bytemuck::PodCastError\">PodCastError</a>","synthetic":false,"types":["bytemuck::PodCastError"]}];
+implementors["livesplit_wrapper"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.56.1/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"livesplit_wrapper/enum.TimerState.html\" title=\"enum livesplit_wrapper::TimerState\">TimerState</a>","synthetic":false,"types":["livesplit_wrapper::TimerState"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ source-files.js
@@ -0,0 +1,4 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["bytemuck"] = {"name":"","files":["contiguous.rs","lib.rs","offset_of.rs","pod.rs","transparent.rs","zeroable.rs"]};
+sourcesIndex["livesplit_wrapper"] = {"name":"","files":["lib.rs"]};
+createSourceSidebar();
