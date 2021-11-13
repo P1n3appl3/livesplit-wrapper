16aa0421bcbf95f5b764387938ef6d707724fb84
Joseph Ryan <josephryan3.14@gmail.com>
2021-11-13 01:29:08 -0800

    Add github actions

--- src/lib.rs
+++ src/lib.rs
@@ -51,8 +51,9 @@ use bytemuck::Pod;
 
 /// Wires up the necessary c interface for a type that implements [`Splitter`].
 ///
-/// If you defined `struct MySplitter {...}` and `impl Splitter for MySplitter {...}` then
-/// just write `register_autosplitter!(MySplitter);` and you'll be good to go.
+/// If you defined `struct MySplitter {...}` and `impl Splitter for MySplitter
+/// {...}` then just write `register_autosplitter!(MySplitter);` and you'll be
+/// good to go.
 #[macro_export]
 macro_rules! register_autosplitter {
     ($struct:ident) => {
@@ -67,7 +68,8 @@ macro_rules! register_autosplitter {
     };
 }
 
-/// Currently the only possible error is a failed memory read on the attached process.
+/// Currently the only possible error is a failed memory read on the attached
+/// process.
 #[derive(Debug)]
 pub enum Error {
     /// A memory read on the attached process failed
@@ -77,34 +79,33 @@ pub enum Error {
 type Result<T> = std::result::Result<T, Error>;
 /// An address in the attached processes memory.
 ///
-/// Autosplitters can attach to 32-bit processes, they'll just get an error if they try to
-/// read outside it's address space.
+/// Autosplitters can attach to 32-bit processes, they'll just get an error if
+/// they try to read outside it's address space.
 pub type Address = u64;
 
-/// A handle representing an attached process that can be used to read its memory.
+/// A handle representing an attached process that can be used to read its
+/// memory.
 #[derive(Debug)]
 pub struct Process(u64);
 
 impl Process {
-    /// Reads a single value from the attached processes memory space. To be able to use
-    /// this with your own types, they need to implement [`Pod`] (it's implemented for the
-    /// numeric types and fixed size arrays by default).
+    /// Reads a single value from the attached processes memory space. To be
+    /// able to use this with your own types, they need to implement [`Pod`]
+    /// (it's implemented for the numeric types and fixed size arrays by
+    /// default).
     pub fn read<T: Pod>(&self, addr: Address) -> Result<T> {
         unsafe {
             let mut buf = MaybeUninit::uninit();
             self.read_into_buf(
                 addr,
-                slice::from_raw_parts_mut(
-                    buf.as_mut_ptr() as *mut u8,
-                    mem::size_of::<T>(),
-                ),
+                slice::from_raw_parts_mut(buf.as_mut_ptr() as *mut u8, mem::size_of::<T>()),
             )?;
             Ok(buf.assume_init())
         }
     }
 
-    /// Search for a module (aka dynamic library) loaded by the attached process by name
-    /// and return its base address.
+    /// Search for a module (aka dynamic library) loaded by the attached process
+    /// by name and return its base address.
     pub fn module(&self, name: &str) -> Option<Address> {
         unsafe {
             match ffi::get_module(self.0, name.as_ptr() as u32, name.len() as u32) {
@@ -114,7 +115,8 @@ impl Process {
         }
     }
 
-    /// Read bytes from the attached processes memory space starting at `addr` into `buf`.
+    /// Read bytes from the attached processes memory space starting at `addr`
+    /// into `buf`.
     pub fn read_into_buf(&self, addr: Address, buf: &mut [u8]) -> Result<()> {
         unsafe {
             (ffi::read_mem(self.0, addr, buf.as_mut_ptr() as u32, buf.len() as u32) != 0)
@@ -134,36 +136,41 @@ impl Drop for Process {
 
 /// The main autosplitter trait.
 ///
-/// This trait is the entry point for the autosplitter's functionality. The `new` and
-/// `update` functions are hooks that will be called by LiveSplit. To interact with
-/// LiveSplit's timer, use the functions defined in [`HostFunctions`].
+/// This trait is the entry point for the autosplitter's functionality. The
+/// `new` and `update` functions are hooks that will be called by LiveSplit. To
+/// interact with LiveSplit's timer, use the functions defined in
+/// [`HostFunctions`].
 ///
-/// [`HostFunctions`] is automatically implemented on `Splitter`s, so in your `update`
-/// function you can call methods like [`self.split()`](HostFunctions::split) and
+/// [`HostFunctions`] is automatically implemented on `Splitter`s, so in your
+/// `update` function you can call methods like
+/// [`self.split()`](HostFunctions::split) and
 /// [`self.set_game_time()`](HostFunctions::set_game_time).
 ///
 /// ## REMEMBER!
 ///
-/// Make sure you use the [`register_autosplitter!`] macro on your splitter! Without it,
-/// your wasm library won't expose the proper functions and it'll fail to load.
+/// Make sure you use the [`register_autosplitter!`] macro on your splitter!
+/// Without it, your wasm library won't expose the proper functions and it'll
+/// fail to load.
 pub trait Splitter {
-    /// Called when the LiveSplit runtime instantiates your splitter. It's a good
-    /// time to attach to a game process, set initial variables, or change your tick
-    /// rate. Note that this _won't_ be called every time the run is reset.
+    /// Called when the LiveSplit runtime instantiates your splitter. It's a
+    /// good time to attach to a game process, set initial variables, or
+    /// change your tick rate. Note that this _won't_ be called every time
+    /// the run is reset.
     fn new() -> Self;
 
-    /// Called periodically by the LiveSplit runtime. To change the rate that it's called,
-    /// use [`set_tick_rate`](HostFunctions::set_tick_rate)
+    /// Called periodically by the LiveSplit runtime. To change the rate that
+    /// it's called, use [`set_tick_rate`](HostFunctions::set_tick_rate)
     fn update(&mut self);
 }
 
 /// The autosplitter's interface for interacting with the LiveSpilit timer.
 pub trait HostFunctions {
-    /// Output a message. This can be used for debugging and/or sending error messages to
-    /// the player through whichever LiveSplit frontend they're using. Note that because
-    /// autosplitters run in WASM, they don't have access to STDOUT or files, so
-    /// typical solutions like `println!` and logging will not work (this could chagne
-    /// in the future as LiveSplit plans to support WASI).
+    /// Output a message. This can be used for debugging and/or sending error
+    /// messages to the player through whichever LiveSplit frontend they're
+    /// using. Note that because autosplitters run in WASM, they don't have
+    /// access to STDOUT or files, so typical solutions like `println!` and
+    /// logging will not work (this could chagne in the future as LiveSplit
+    /// plans to support WASI).
     fn print(&self, str: &str) {
         unsafe { ffi::print_message(str.as_ptr(), str.len()) }
     }
@@ -178,16 +185,18 @@ pub trait HostFunctions {
         }
     }
 
-    /// Start the timer for a run. Note that this will silently do nothing on subsequent
-    /// calls. To start a new run, call `reset()` and _then_ `start()`.
+    /// Start the timer for a run. Note that this will silently do nothing on
+    /// subsequent calls. To start a new run, call `reset()` and _then_
+    /// `start()`.
     fn start(&self) {
         unsafe { ffi::start() }
     }
 
-    /// Pause the game time counter. This is often used when entering a loading screen or
-    /// end level screen for games that use in game time rather than real time. It may
-    /// be a good idea to call `set_game_time()` immediately after pausing so that
-    /// LiveSplit's game time counter shows the exact current time.
+    /// Pause the game time counter. This is often used when entering a loading
+    /// screen or end level screen for games that use in game time rather
+    /// than real time. It may be a good idea to call `set_game_time()`
+    /// immediately after pausing so that LiveSplit's game time counter
+    /// shows the exact current time.
     fn pause(&self) {
         unsafe { ffi::pause_game_time() }
     }
@@ -202,33 +211,35 @@ pub trait HostFunctions {
         unsafe { ffi::split() }
     }
 
-    /// Reset the run. Don't do this automatically when a run has finished, and in general
-    /// be conservative about resetting runs from the autosplitter. Common practice is to
-    /// do so only if there's an unambiguous signal that the player is done with this run.
+    /// Reset the run. Don't do this automatically when a run has finished, and
+    /// in general be conservative about resetting runs from the
+    /// autosplitter. Common practice is to do so only if there's an
+    /// unambiguous signal that the player is done with this run.
     fn reset(&self) {
         unsafe { ffi::reset() }
     }
 
-    /// Set the game time. Note that if the timer is not paused, the time shown will keep
-    /// incrementing immediately after it is set to the given value.
+    /// Set the game time. Note that if the timer is not paused, the time shown
+    /// will keep incrementing immediately after it is set to the given
+    /// value.
     fn set_game_time(&self, time: f64) {
         unsafe { ffi::set_game_time(time) }
     }
 
-    /// Set the rate at which the [`update`](Splitter::update) function will be called (in
-    /// Hz).
+    /// Set the rate at which the [`update`](Splitter::update) function will be
+    /// called (in Hz).
     fn set_tick_rate(&self, rate: f64) {
         unsafe { ffi::set_tick_rate(rate) }
     }
 
-    /// Get the current state of the timer. This is how the autosplitter can detect if the
-    /// player manually paused or reset a run.
+    /// Get the current state of the timer. This is how the autosplitter can
+    /// detect if the player manually paused or reset a run.
     fn state(&self) -> TimerState {
         unsafe { std::mem::transmute(ffi::get_timer_state() as u8) }
     }
 
-    /// Set a variable which can be displayed by LiveSplit. This is commonly used for
-    /// features like death counters.
+    /// Set a variable which can be displayed by LiveSplit. This is commonly
+    /// used for features like death counters.
     fn set_variable(&self, key: &str, value: &str) {
         unsafe {
             ffi::set_variable(
