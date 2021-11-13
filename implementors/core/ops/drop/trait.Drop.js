7353b9228d347d2d13248d3effe002efcd88010c
P1n3appl3 <P1n3appl3@users.noreply.github.com>
2021-11-13 11:08:20 +0000

    deploy: 61f0675acab3a1c94a0ef5a95f1116575326c774

--- implementors/core/ops/drop/trait.Drop.js
+++ implementors/core/ops/drop/trait.Drop.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["livesplit_wrapper"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.56.1/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"livesplit_wrapper/struct.Process.html\" title=\"struct livesplit_wrapper::Process\">Process</a>","synthetic":false,"types":["livesplit_wrapper::Process"]}];
+implementors["livesplit_wrapper"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"livesplit_wrapper/struct.Process.html\" title=\"struct livesplit_wrapper::Process\">Process</a>","synthetic":false,"types":["livesplit_wrapper::Process"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
